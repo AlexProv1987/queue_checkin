8518bea0767b7d89dd20122dc9456f41b0899da4
AlexProv1987 <alex.provenzano87@gmail.com>
2025-10-22 17:39:42 -0500

    ui updates and now dropdown working

---
+++ src/x-1210821-queue-checkin/actions.js
@@ -0,0 +1,6 @@
+import {actionTypes} from '@servicenow/ui-core';
+import {createHttpEffect} from '@servicenow/ui-effect-http';
+
+export default {
+    
+}
--- src/x-1210821-queue-checkin/index.js
+++ src/x-1210821-queue-checkin/index.js
@@ -1,6 +1,6 @@
 //https://developer.servicenow.com/dev.do#!/reference/next-experience/yokohama/now-components/now-button/overview#slots
 //https://github.com/ServiceNowDevProgram/now-experience-component-examples/blob/quebec/src/checklist-example/example-checklist/actions.js
-
+//https://www.servicenow.com/community/next-experience-articles/component-and-data-resource-properties-in-ui-builder/ta-p/2331894
 import { createCustomElement, actionTypes, declarativeOperations } from '@servicenow/ui-core';
 import snabbdom from '@servicenow/ui-renderer-snabbdom';
 import '@servicenow/now-button';
@@ -8,84 +8,89 @@ import '@servicenow/now-loader';
 import '@servicenow/now-alert';
 import '@servicenow/now-icon';
 import '@servicenow/now-card';
+import '@servicenow/now-dropdown';
 import styles from './styles.scss';
 import { createHttpEffect } from '@servicenow/ui-effect-http';
 
-//I am opting to not save the user in state or props currently as I dont want a re render. - i havnt found the equilivant of a ref for react yet that doesnt cause a re render.
-
 const view = (state, { updateState, dispatch }) => {
 
-	const { items, user, loadingItems, errors } = state;
+	const { items, loadingItems, errors } = state;
 
 	return (
 		<main>
-			<div className="header">
-				<div className='header__title'>My Queue Capacity</div>
-			</div>
-			<div>
-				{loadingItems ? (
-					<now-loader />
-				) : (
-					<div className="card-grid">
-						{items.map((item, index) => (
-							<now-card size="sm" key={index}>
-								<now-card-header
-									style={{ marginTop: '10px' }}
-									tagline={{ icon: 'document-outline', label: 'Queue' }}
-									heading={{ label: item.channel.display_value, size: 'md', }} />
-								<now-card-divider full-width />
-								<div
-									style={{
-										display: 'flex',
-										justifyContent: 'center',
-										alignItems: 'center',
-										padding: '5px 0'
-									}}
-								>
-									<now-dropdown
-										tooltip-content="Select Capacity"
-										select="single"
-										config-label={{ label: 'Capacity' }}
-										config-aria={{
-											trigger: { 'aria-label': 'Select an item' },
-											panel: { 'aria-label': 'Assigned to' }
-										}}
-										items={[
-											{ id: '0', label: '0' },
-											{ id: '1', label: '1' },
-											{ id: '2', label: '2' },
-											{ id: '3', label: '3' },
-											{ id: '4', label: '4' },
-											{ id: '5', label: '5' }
-										]}
-										selected-items={[String(item.max_capacity ?? 1)]}
-										on-change={(e) =>
-											dispatch('UPDATE_CAPACITY', {
-												index,
-												value: Number(e.detail.selectedItems[0])
-											})
-										}
-										style={{
-											width: '80%',
-											margin: '8px 0',
-											'--now-dropdown-min-width': '100%'
-										}}
-									></now-dropdown>
-								</div>
-								<now-card-divider full-width />
-								<now-card-footer label={{ start: 'Last Updated', end: `${item.sys_updated_on}` }} />
-							</now-card>
+			<div className='page'>
+				{/**error/success msgs here */}
+				<div className="header">
+					<div className="header__title">My Queue Capacity</div>
 
-						))}
+					<div className="header__actions">
+						<now-button
+							tooltip-content="Save Changes"
+							size="md"
+							variant="primary"
+							label="Submit"
+						/>
+						<now-button
+							tooltip-content="Reset to default for all queues"
+							size="md"
+							variant="primary"
+							label="Reset"
+						/>
+						<now-button
+							tooltip-content="Sets all queues to 0 Capacity"
+							size="md"
+							variant="primary"
+							label="Check Out"
+						/>
 					</div>
-				)}
-			</div>
-			<div style={{paddingTop:'10px'}}>
-				<now-button
-					size='md'
-					variant='primary'
-					label='Submit'
-			/*this by default calls the now_button#clicked action handler*/ />
+				</div>
+				<div>
+					{loadingItems ? (
+						<now-loader />
+					) : (
+						<div className="card-grid">
+							{items.map((item, index) => (
+								<now-card size="sm" key={index} sidebar={{ variant: 'primary' }}>
+									<now-card-header
+										style={{ marginTop: '10px' }}
+										tagline={{ icon: 'document-outline', label: 'Queue' }}
+										heading={{ label: item.channel.display_value, size: 'md', }} />
+									<now-card-divider full-width />
+									<div
+										style={{
+											display: 'flex',
+											justifyContent: 'center',
+											alignItems: 'center',
+											padding: '5px 0'
+										}}
+									>
+										<now-dropdown
+											id={item.sys_id}
+											tooltip-content="Select Capacity"
+											select="single"
+											config-label={{ label: 'Capacity' }}
+											config-aria={{
+												trigger: { 'aria-label': 'Select an item' },
+												panel: { 'aria-label': 'Assigned to' }
+											}}
+											items={[0, 1, 2, 3, 4, 5].map(capacity => ({ id: `${item.sys_id}|${capacity}`, label: String(capacity) }))}
+											selected-items={[`${item.sys_id}|${item.max_capacity}`]}
+											manage-selected-items
+											style={{
+												width: '80%',
+												margin: '8px 0',
+												'--now-dropdown-min-width': '100%'
+											}}
+										></now-dropdown>
+									</div>
+									<now-card-divider full-width />
+									<now-card-footer label={{ start: 'Last Updated', end: `${item.sys_updated_on}` }} />
+								</now-card>
+
+							))}
+						</div>
+					)}
+				</div>
 			</div>
 		</main>
 	)
@@ -106,14 +111,6 @@ createCustomElement('x-1210821-queue-checkin', {
 		//runs on mount
 		[actionTypes.COMPONENT_BOOTSTRAPPED]: ({ dispatch }) => {
 			dispatch('HTTP_FETCH_USER');
-		},
-
-		'UPDATE_CAPACITY': ({ action, state, updateState }) => {
-			const { index, value } = action.payload;
-			const updated = state.items.map((item, i) =>
-				i === index ? { ...item, max_capacity: value } : item
-			);
-			updateState({ items: updated });
 		},
 
 		HTTP_FETCH_USER: createHttpEffect('api/now/ui/user/current_user', {
@@ -146,20 +143,37 @@ createCustomElement('x-1210821-queue-checkin', {
 			})
 		},
 
-		FETCH_USER_ERROR: ({ action, updateState }) => {
+		FETCH_USER_ERROR: ({ action, updateState, }) => {
 			console.log(action.payload?.error?.message || 'Request failed')
 			const msg = action.payload?.error?.message || 'Request failed';
 			updateState({ loadingUsers: false, usersError: msg });
 		},
 
-
 		//default onclick event for now-button
-		'NOW_BUTTON#CLICKED': ({ dispatch }) => {
+		'NOW_BUTTON#CLICKED': ({ dispatch, action, state }) => {
 			console.log('clicked btn')
-			dispatch('HTTP_FETCH_ITEMS', {
-				sysparm_limit: 10,
-				sysparm_query: 'active=true'
-			});
+			const { items } = state
+			console.log(items)
+		},
+
+		//default handler for selected item
+		'NOW_DROPDOWN#SELECTED_ITEMS_SET': ({ action, updateState, state }) => {
+			const payload = action.payload?.value?.[0];
+
+			if (!payload)
+				return
+
+			const [sys_id, valueStr] = payload.split('|');
+			const value = valueStr;
+
+			if (!sys_id || !value)
+				return;
+
+			const items = state.items.map((it) =>
+				it.sys_id === sys_id ? { ...it, max_capacity: value } : it
+			);
+
+			updateState({ items });
 		},
 
 		HTTP_FETCH_ITEMS: createHttpEffect('/api/now/table/awa_agent_capacity', {
@@ -170,7 +184,6 @@ createCustomElement('x-1210821-queue-checkin', {
 		}),
 
 		HTTP_FETCH_ITEMS_SUCCESS: ({ action, updateState }) => {
-			console.log('results array:', action.payload.result)
 			updateState({ items: action.payload.result, loadingItems: false });
 		},
 
